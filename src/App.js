cbc652685d1bbb508490c4118428b80e599b97f2
Sangwon Hong <qpakzk@gmail.com>
2018-02-17 02:41:55 +0900

    render movies from YTS
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Movie from './Movie'
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  state = {}
+
+  componentDidMount() {
+    this._getMovies()
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+    })
+
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
