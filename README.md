2c7babfd7e7ee665027a36689c76d528e395d5a9
Sangwon Hong <qpakzk@gmail.com>
2018-02-17 16:43:56 +0900

    Complete movie-app made with React JS
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
   * Movie App made with React JS
   * React JS Fundamentals Course on Nomad Academy instructed by Nicolás
+
+## Web Page
+
+  https://qpakzk.github.io/movie-app/
--- src/App.js
+++ src/App.js
@@ -13,28 +13,38 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie 
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
-
     return movies
   }
 
   _getMovies = async () => {
     const movies = await this._callApi()
+    console.log(movies)
     this.setState({
       movies
     })
   }
 
   _callApi = () => {
-    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-    .then(potato => potato.json())
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+    .then(response => response.json())
     .then(json => json.data.movies)
     .catch(err => console.log(err))
   }
+
   render() {
+    const { movies } = this.state
     return (
-      <div className="App">
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     )
